754bc110989a14ebaec1789b81e29f6a8da42585
0miik0 <danielajarosova182@seznam.cz>
2024-01-31 12:23:09 +0100

    aaa

--- src/Okno.java
+++ src/Okno.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Okno extends JFrame{
 
-    private JTextField textField1;
+    private JTextField tfNazev;
     private JButton btn1;
     private JButton btn2;
     private JButton btn3;
@@ -16,10 +16,21 @@ public class Okno extends JFrame{
         setContentPane(oknoMain);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400, 300);
+        zobrazDeskovku();
+        btn1.addActionListener(e -> {
+            indexAktualniHry--;
+            zobrazDeskovku();
+        });
+        btn3.addActionListener(e -> {
+            indexAktualniHry++;
+            zobrazDeskovku();
+        });
     }
 
     public void zobrazDeskovku(){
+        //osetrit kdyz je seznam prazdny - if seznam prazdny
         Deskovka aktualniDeskovka = model.getDeskovka(indexAktualniHry);
+        tfNazev.setText(aktualniDeskovka.getNazev());
     }
 
     public static void main(String[] args) {
